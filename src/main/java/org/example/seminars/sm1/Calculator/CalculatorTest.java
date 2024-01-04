387661565e8a5b71f90e4f5ddc6ea590acd424e4
Maksim <i@mpozdnjakov.ru>
2024-01-04 13:50:43 +0300

    hw2

--- src/main/java/org/example/seminars/sm1/Calculator/CalculatorTest.java
+++ src/main/java/org/example/seminars/sm1/Calculator/CalculatorTest.java
@@ -5,26 +5,26 @@ import static org.assertj.core.api.Assertions.*;
 public class CalculatorTest {
     public static void main(String[] args) {
         // Проверка базового функционала с целыми числами:
-//        if (8 != Calculator.calculation(2, 6, '+')) {
+//        if (8 != org.example.seminars.sm2.second.Calculator.calculation(2, 6, '+')) {
 //            throw new AssertionError("Ошибка в методе");
 //        }
 //
-//        if (0 != Calculator.calculation(2, 2, '-')) {
+//        if (0 != org.example.seminars.sm2.second.Calculator.calculation(2, 2, '-')) {
 //            throw new AssertionError("Ошибка в методе");
 //        }
 //
-//        if (14 != Calculator.calculation(2, 7, '*')) {
+//        if (14 != org.example.seminars.sm2.second.Calculator.calculation(2, 7, '*')) {
 //            throw new AssertionError("Ошибка в методе");
 //        }
 //
-//        if (2 != Calculator.calculation(100, 50, '/')) {
+//        if (2 != org.example.seminars.sm2.second.Calculator.calculation(100, 50, '/')) {
 //            throw new AssertionError("Ошибка в методе");
 //        }
 
         // Случаи с неправильными аргументами
         // аргумент operator типа char, должен вызывать исключение, если он получает не базовые символы (+-*/)
 //         try {
-//             Calculator.calculation(8, 4, '_');
+//             org.example.seminars.sm2.second.Calculator.calculation(8, 4, '_');
 //         } catch (IllegalStateException e) {
 //             if (!e.getMessage().equals("Unexpected value operator: _")) {
 //                 throw new AssertionError("Ошибка в методе");
@@ -32,10 +32,10 @@ public class CalculatorTest {
 //         }
 
         // Проверка базового функционала с целыми числами, с использованием утверждений:
-//        assert 8 == Calculator.calculation(2, 6, '+');
-//        assert 0 == Calculator.calculation(2, 2, '-');
-//        assert 14 == Calculator.calculation(2, 7, '*');
-//        assert 2 == Calculator.calculation(100, 50, '/');
+//        assert 8 == org.example.seminars.sm2.second.Calculator.calculation(2, 6, '+');
+//        assert 0 == org.example.seminars.sm2.second.Calculator.calculation(2, 2, '-');
+//        assert 14 == org.example.seminars.sm2.second.Calculator.calculation(2, 7, '*');
+//        assert 2 == org.example.seminars.sm2.second.Calculator.calculation(100, 50, '/');
 
         // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
         assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
@@ -48,28 +48,28 @@ public class CalculatorTest {
                 Calculator.calculation(8, 4, '_')
         ).isInstanceOf(IllegalStateException.class);
 //
-//        System.out.println(Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
-//        System.out.println(Calculator.squareRootExtraction(169));
+//        System.out.println(org.example.seminars.sm2.second.Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
+//        System.out.println(org.example.seminars.sm2.second.Calculator.squareRootExtraction(169));
 
         // Примерные решения домашних заданий из 1 лекции:
 
         // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
         // необходимые проверки для него используя граничные случаи
         // assertThatThrownBy(() ->
-        //         seminars.first.Calculator.Calculator.squareRootExtraction(0, 1, -1)
+        //         seminars.first.org.example.seminars.sm2.second.Calculator.org.example.seminars.sm2.second.Calculator.squareRootExtraction(0, 1, -1)
         // ).isInstanceOf(SomeStateException.class);
 
         // HW1.2: Как будет выглядеть проверка для случая деления на ноль? (с использованием AssertJ)
         // assertThatThrownBy(() ->
-        //        seminars.first.Calculator.Calculator.calculation(5, 0, '/')
+        //        seminars.first.org.example.seminars.sm2.second.Calculator.org.example.seminars.sm2.second.Calculator.calculation(5, 0, '/')
         // ).isInstanceOf(ArithmeticException.class);
 
         // HW1.3: Сравните одну и ту же проверку с использованием условий, ассертов, AssertJ
         // в каком случае стандартное сообщение об ошибке будет более информативным?
-        // if (0 != seminars.first.Calculator.Calculator.calculation(2, 6, '+')) {
+        // if (0 != seminars.first.org.example.seminars.sm2.second.Calculator.org.example.seminars.sm2.second.Calculator.calculation(2, 6, '+')) {
         //     throw new AssertionError("Ошибка в методе");
         // }
-        //   assert 0 == seminars.first.Calculator.Calculator.calculation(2, 6, '+');
-        //    assertThat(seminars.first.Calculator.Calculator.calculation(2, 6, '+')).isEqualTo(0);
+        //   assert 0 == seminars.first.org.example.seminars.sm2.second.Calculator.org.example.seminars.sm2.second.Calculator.calculation(2, 6, '+');
+        //    assertThat(seminars.first.org.example.seminars.sm2.second.Calculator.org.example.seminars.sm2.second.Calculator.calculation(2, 6, '+')).isEqualTo(0);
     }
 }
